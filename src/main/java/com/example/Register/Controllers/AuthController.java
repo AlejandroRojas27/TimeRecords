eaefdcc4147efec7255df67535a4b59970122c56
Alejandro Rojas <rmae2727@gmail.com>
2024-05-27 17:21:44 -0600

    Clean up the code

--- src/main/java/com/example/Register/Controllers/AuthController.java
+++ src/main/java/com/example/Register/Controllers/AuthController.java
@@ -8,6 +8,8 @@ import com.example.Register.Utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 public class AuthController {
 
@@ -18,23 +20,29 @@ public class AuthController {
     private UserRepository userRepository;
 
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
-    public boolean login(@RequestBody UserModel user){
+    public boolean login(@RequestBody UserModel user) {
         UserModel userLogged = userRepository.findUserByIdentifications(user);
         return userLogged != null;
     }
 
     @RequestMapping(value = "api/authLogin", method = RequestMethod.POST)
-    public String authLogin(@RequestBody UserModel user){
+    public String authLogin(@RequestBody UserModel user) {
         UserModel userlogged = userRepository.findUserByIdentifications(user);
 
-        if(userlogged != null){
-            String token = jwtUtil.generateToken(String.valueOf(userlogged.getCredential()));
+        if (userlogged != null) {
+
+            String subject = String.valueOf(userlogged.getCredential());
+            String fullName = String.valueOf(userlogged.getFirstName()) + " " + String.valueOf(userlogged.getLastName());
+
+            String token = jwtUtil.generateToken(subject, fullName);
             return token;
         }
         return "FAIL";
     }
 
     @RequestMapping(value = "api/authToken")
-    public boolean tokenIsValid(@RequestHeader(value = "Authorization") String token){return jwtUtil.validateToken(token);}
+    public boolean tokenIsValid(@RequestHeader(value = "Authorization") String token) {
+        return jwtUtil.validateToken(token);
+    }
 
 }
